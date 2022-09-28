db2e73e8431028d1f618c318109f2fc6f655f9a7
Imran Zukorlic <mr.silver020@gmail.com>
2022-09-28 17:14:52 +0200

    work

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -1,5 +1,24 @@
 import React from "react";
+import products from "../../common/items.json";
+import ProductCard from "../../components/ProductCard/ProductCard";
+import { ShoppingCartContext } from "../../context/ShoppingCartContext";
+import { useContext } from "react";
 
 export default function AboutPage() {
-  return <div>Welcome to about page</div>;
+  const { addToCart } = useContext(ShoppingCartContext);
+  return (
+    <div className="flex flex-wrap justify-center items-baseline py-16 bg-sky-400 ">
+      {products.map((product) => (
+        <div className="flex basis-1/4 justify-center items-center mt-10">
+          <ProductCard
+            key={product.id}
+            price={product.price}
+            image={product.imgUrl}
+            title={product.name}
+            onClick={() => addToCart(product)}
+          />
+        </div>
+      ))}
+    </div>
+  );
 }
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,39 +2,47 @@ import React, { useContext } from "react";
 import ProductCard from "../../components/ProductCard/ProductCard";
 import products from "../../common/items.json";
 import { ShoppingCartContext } from "../../context/ShoppingCartContext";
+import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
   const { addToCart } = useContext(ShoppingCartContext);
+  const navigate = useNavigate();
   return (
     <>
-      <div className="flex w-full justify-center mt-5 h-1/5 flex-row  ">
+      <div className="flex w-full justify-center py-5 h-1/5 flex-row bg-sky-200 ">
         <img
-          className=" flex  w-7/12 h-1/5 "
-          src="https://th.bing.com/th/id/OIP.HoYmNK9LK2rc2mUetSgcBAHaDP?w=350&h=153&c=7&r=0&o=5&dpr=1.3&pid=1.7"
-          alt="slika"
-        />
-        <img
-          className=" flex  w-3/12 h-1/5 border-4 border-cyan-400 "
-          src="https://th.bing.com/th/id/OIP.3aOwtWG3J2um24yqyeyOXwHaHa?w=203&h=203&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+          className=" flex  w-8/12 h-1/5 "
+          src="https://as1.ftcdn.net/v2/jpg/02/52/68/04/1000_F_252680454_05E8u8lQijA3nn4MloY0sDn1tVDn9YWz.jpg"
           alt="slika"
         />
       </div>
-
-      <div className="flex items-center mt-10 justify-center border-4 w-1/6 h-12 border-sky-800 rounded-xl bg-sky-800  text-amber-50 ease-out duration-300 hover:bg-white hover:text-black">
-        <h3 className="font-serif text-xl font-extrabold ">
-          Najbolje u ponudi
-        </h3>
+      <div className="flex items-center justify-center bg-sky-300">
+        <div className="flex items-center my-12  justify-center border-4 w-1/6 h-12 border-sky-800 rounded-xl bg-sky-800  text-amber-50 ease-out duration-300 hover:bg-white hover:text-black">
+          <h3 className="font-serif text-xl font-extrabold ">
+            ↓ Najbolje u ponudi ↓
+          </h3>
+        </div>
       </div>
-      <div className="flex justify-center items-center">
-        {products.map((product) => (
-          <ProductCard
-            key={product.id}
-            price={product.price}
-            image={product.imgUrl}
-            title={product.name}
-            onClick={() => addToCart(product)}
-          />
-        ))}
+      <div className="flex items-center justify-evenly py-16 bg-sky-400 ">
+        {products
+          .map((product) => (
+            <ProductCard
+              key={product.id}
+              price={product.price}
+              image={product.imgUrl}
+              title={product.name}
+              onClick={() => addToCart(product)}
+            />
+          ))
+          .slice(0, 4)}
+      </div>
+      <div className="flex items-center justify-center h-28 bg-sky-500">
+        <button
+          onClick={() => navigate("/about")}
+          className="flex items-center my-12 font-serif text-xl font-extrabold justify-center border-4 w-1/6 h-12 border-sky-800 rounded-xl bg-sky-800  text-amber-50 ease-out duration-300 hover:bg-white hover:text-black"
+        >
+          Pogledaj vise ...
+        </button>
       </div>
     </>
   );
